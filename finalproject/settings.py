e5403348b11430a34769464de40b044547c3ac27
vidushi2504 <49758900+vidushi2504@users.noreply.github.com>
2021-05-03 18:25:46 +0530

    Update settings.py

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -148,7 +148,7 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER='info.developershub@gmail.com'
-EMAIL_HOST_PASSWORD='developershub'
+#Add email EMAIL_HOST_USER=''
+#Add password EMAIL_HOST_PASSWORD=''
 
 
